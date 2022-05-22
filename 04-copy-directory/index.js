be86382c64bd63f8027bf87bd763371f214b6729
aleksa-mnk <alyonamankevich@gmail.com>
2022-05-22 14:21:54 +0300

    feat: complete 04-copy-in-folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const path = require('path');
+const fs = require('fs/promises');
 
+const dirPathIn = path.join(__dirname, 'files');
+const dirPathOut = path.join(__dirname, 'files-copy');
+
+const copyDirectory = async () => {
+    await fs.rm(dirPathOut, { force: true, recursive: true });
+    await fs.mkdir(dirPathOut);
+    const files = await fs.readdir(dirPathIn, { withFileTypes: true });
+    files.forEach(async (file) => {
+        if (file.isFile()) {
+            const filePathIn = path.join(dirPathIn, file.name);
+            const filePathOut = path.join(dirPathOut, file.name);
+            await fs.copyFile(filePathIn, filePathOut);
+        }
+    });
+};
+
+copyDirectory();

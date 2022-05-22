b6dd3e300288aaecbb128beac516e3b414495025
aleksa-mnk <alyonamankevich@gmail.com>
2022-05-22 12:44:56 +0300

    feat: complete 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const pathText = path.join(__dirname, 'text.txt');
-const ReadStream  = fs.createReadStream(pathText, 'utf-8');
+const ReadStream = fs.createReadStream(pathText, 'utf-8');
 
 ReadStream.on('data', chunk => console.log(chunk));
 ReadStream.on('error', error => console.log(error.message));
